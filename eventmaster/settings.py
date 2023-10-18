c7702cc89613b8d70e1a6d4d8156c8523e1843b9
Mehedi Hasan Tayef <tayef05@outlook.com>
2023-10-18 10:16:14 +0600

    Add core app with user auth models and admin
    This commit introduces the core app to the project, which includes the following changes:
    
    1. User Authentication Models:
        - Extends the user model from AbstractUser.
        - Adds a unique email field to the user model.
        - Sets `core.User` as the `AUTH_USER_MODEL` in the project's settings.
    
    2. Admin Interfaces:
        - Registers the user model as `UserAdmin`.
        - Extends the `UserAdmin` class.
        - Customizes admin views for user management.
    
    These changes are foundational for user management and authentication within the project.

--- eventmaster/settings.py
+++ eventmaster/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'eventmaster.urls'
@@ -123,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ["127.0.0.1",]
+
+AUTH_USER_MODEL = 'core.User'
